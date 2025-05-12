5a06557d5bbb2ce930d1e02cdc140afcfa80c858
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2025-05-12 19:12:54 +0700

    add: configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+  .setTitle('E-commerce API')
+  .setDescription('The e-commerce API description')
+  .setVersion('1.0')
+  .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
